9a16488c6c4433f91f9ee66e28415a888cb71951
Takumi Ogawa <taogawa@ucsd.edu>
2026-03-27 17:59:47 -0700

    Fix url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const handlePredict = async () => {
 
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://mental-health-risk-prediction-2p2w.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
